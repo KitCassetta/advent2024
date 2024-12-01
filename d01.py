9a7110eccdb9f9680ae8157c4aa3b726adfea118
Kit Cassetta <KitCassetta@gmail.com>
2024-12-01 11:57:23 -0500

    advent of code 2024 day 1

---
+++ d01.py
@@ -0,0 +1,31 @@
+import csv
+from collections import Counter
+
+from operator import length_hint
+
+l1 = []
+l2 = []
+distance = []
+with open("d01.txt", 'r') as fi:
+    csv_reader = csv.reader(fi)
+    for row in csv_reader:
+        l1.append(int(row[0]))
+        l2.append(int(row[-1]))
+
+l1.sort()
+l2.sort()
+
+for i in range(len(l1)):
+    if l1[i] < l2[i]:
+        distance.append(l2[i]-l1[i])
+    else:
+        distance.append(l1[i]-l2[i])
+
+print(sum(distance))
+
+score  = []
+cnt = Counter(l2)
+for i in l1:
+    score.append(i * cnt[i])
+
+print(sum(score))
